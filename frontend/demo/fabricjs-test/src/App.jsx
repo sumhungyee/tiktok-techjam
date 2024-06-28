7765c333306023ee201ffe3bcaa0095432b4671f
shotnothing <shotnothing@protonmail.ch>
2024-06-28 22:55:37 +0800

    feat(frontend): basic fabricjs demo added

---
+++ frontend/demo/fabricjs-test/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import FabricCanvas from './components/FabricJSViewport'
+
+function App() {
+  return (
+    <>
+      <FabricCanvas />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/demo/fabricjs-test/src/components/FabricJSViewport.jsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { fabric } from 'fabric';
+
+// Will be served via API later
+import item1 from '../assets/Pleated Skirt mimi momo.png';
+import item2 from '../assets/SHEIN EZwear Cartoon & Slogan Graphic Crop Tee.png';
+import item3 from "../assets/SHEIN EZwear Women's Drawstring Side Asymmetrical Hem Summer Tube Top.png";
+import item4 from "../assets/SHEIN MOD Ladies' Fashionable Asymmetrical Strap Ruffle Top, Light Yellow, Ideal For Summer Vacation.png";
+import item5 from "../assets/SHEIN MOD Women's Floral Print Shirred Wide Strap Tank Top.png";
+
+const FabricCanvas = () => {
+    const [canvas, setCanvas] = useState();
+
+    useEffect(() => {
+        const c = new fabric.Canvas("canvas", {
+          height: 932,
+          width: 430,
+          backgroundColor: "white",
+        });
+    
+        //fabric.Object.prototype.cornerStyle = "rect";
+    
+        setCanvas(c);
+        return () => {
+          c.dispose();
+        };
+      }, []);
+
+    const addTop = (canvas) => {
+        const items = [item2, item3, item4, item5];
+        const randomItem = items[Math.floor(Math.random() * items.length)];
+
+        fabric.Image.fromURL(randomItem, (img) => {
+            img.set({
+              left: 0,
+              top: 0,
+              angle: 0,
+              padding: 0,
+              cornersize: 10,
+              hasRotatingPoint: true,
+
+                scaleX: 0.3,
+                scaleY: 0.3,
+
+            });
+            canvas.add(img);
+          });
+    };
+
+    const addBottom = (canvas) => {
+        const items = [item1];
+        const randomItem = items[Math.floor(Math.random() * items.length)];
+
+        fabric.Image.fromURL(randomItem, (img) => {
+            img.set({
+              left: 0,
+              top: 0,
+              angle: 0,
+              padding: 0,
+              cornersize: 10,
+              hasRotatingPoint: true,
+
+                scaleX: 0.3,
+                scaleY: 0.3,
+
+            });
+            canvas.add(img);
+          });
+    };
+
+    const deleteSelected = (canvas) => {
+        const activeObject = canvas.getActiveObject();
+        if (activeObject) {
+          canvas.remove(activeObject);
+        }
+    }
+
+
+    return (
+    <div>
+      <button onClick={() => addTop(canvas)}>Random Top</button>
+      <button onClick={() => addBottom(canvas)}>Random Bottom</button>
+      <button onClick={() => deleteSelected(canvas)}>Delete Selected</button>
+
+      <canvas id="canvas" />
+    </div>
+    );
+};
+
+export default FabricCanvas;
