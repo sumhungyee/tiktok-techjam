34080c2aa75e74c8bbf7bc7b7014bf835d92099e
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2024-06-30 19:39:56 +0800

    modify requirements.txt

--- backend/src/models/model_operations.py
+++ backend/src/models/model_operations.py
@@ -1,21 +1,21 @@
+from PIL import Image, ImageFile
 from io import BytesIO
-
-from PIL import Image
 from rembg import remove
 
 
 def get_image_bytes(image: Image) -> bytes:
+    #ImageFile.LOAD_TRUNCATED_IMAGES = True
     image_byte_array = BytesIO()
-    image.save(image_byte_array, format="png")
+    image.save(image_byte_array, format='png')
     return image_byte_array.getvalue()
 
-
-def load_image_from_bytes(bytes: bytes) -> Image:
+def load_image_from_bytes(bytes: bytes) -> Image.Image:
     image_byte_array = BytesIO(bytes)
     return Image.open(image_byte_array)
-
 
 def remove_background(input_image_bytes: bytes) -> bytes:
     input_image = load_image_from_bytes(input_image_bytes)
     output_image = remove(input_image)
     return get_image_bytes(output_image)
+
+
