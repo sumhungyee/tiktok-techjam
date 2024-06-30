0e43da127aa01bf9abe3accaa7935bd43fdfd345
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2024-06-30 18:02:27 +0800

    make slight modifications to functions

--- backend/src/models/model_operations.py
+++ backend/src/models/model_operations.py
@@ -1,16 +1,19 @@
-from PIL import Image
 from io import BytesIO
+
+from PIL import Image
 from rembg import remove
 
 
 def get_image_bytes(image: Image) -> bytes:
     image_byte_array = BytesIO()
-    image.save(image_byte_array, format='png')
+    image.save(image_byte_array, format="png")
     return image_byte_array.getvalue()
+
 
 def load_image_from_bytes(bytes: bytes) -> Image:
     image_byte_array = BytesIO(bytes)
     return Image.open(image_byte_array)
+
 
 def remove_background(input_image_bytes: bytes) -> bytes:
     input_image = load_image_from_bytes(input_image_bytes)
