fdacda2bc2e8d26fe0eed282c3bedcd486243c79
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2024-07-02 23:50:02 +0800

    add tests

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -6,4 +6,11 @@ def test_get_user_item():
         db.add_item_to_user_wishlist(1, 1)
         db.add_item_to_user_wishlist(1, 2)
         db.add_item_to_user_wishlist(1, 3)
-    assert get_user_item(1, 2).id == 2
+    assert get_user_item(1, 2).id == 2 and get_user_item.user_id == 1
+    assert get_user_item(1, 3).id == 3
+    assert get_user_item(1, 3).user_id == 1
+
+def test_get_shop_item():
+    with DBOperation() as db:
+        db.add_item_to_shop_wardrobe(1, 1, "hi", "$99", "url")
+        assert get_shop_item(1, 1).description == "hi"
