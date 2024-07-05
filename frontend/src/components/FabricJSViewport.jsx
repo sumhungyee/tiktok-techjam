83621e5f79bdee65e63e5335d03916970ab57ae7
Aditya Bajaj <bajaj.aditya2204@gmail.com>
2024-07-05 23:33:16 +0800

    Remove ScrollToTop and setup image to canvas logic

--- frontend/src/components/FabricJSViewport.jsx
+++ frontend/src/components/FabricJSViewport.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { fabric } from 'fabric';
+import React, { useEffect, useRef, useState } from "react";
+import { fabric } from "fabric";
 import {
   Button,
   useDisclosure,
@@ -18,23 +18,29 @@ import {
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
-} from '@chakra-ui/react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { FreeMode, Pagination } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/free-mode';
-import 'swiper/css/pagination';
+} from "@chakra-ui/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 
-import Lists from '../pages/Lists';
+import Lists from "../pages/Lists";
 
 // Will be served via API later
-import item1 from '../assets/Pleated Skirt mimi momo.png';
-import item2 from '../assets/SHEIN EZwear Cartoon & Slogan Graphic Crop Tee.png';
+import item1 from "../assets/Pleated Skirt mimi momo.png";
+import item2 from "../assets/SHEIN EZwear Cartoon & Slogan Graphic Crop Tee.png";
 import item3 from "../assets/SHEIN EZwear Women's Drawstring Side Asymmetrical Hem Summer Tube Top.png";
 import item4 from "../assets/SHEIN MOD Ladies' Fashionable Asymmetrical Strap Ruffle Top, Light Yellow, Ideal For Summer Vacation.png";
 import item5 from "../assets/SHEIN MOD Women's Floral Print Shirred Wide Strap Tank Top.png";
 
-const ListsDrawer = ({ isOpen, onOpen, onClose, drawerTriggerBtnRef }) => {
+const ListsDrawer = ({
+  isOpen,
+  onOpen,
+  onClose,
+  drawerTriggerBtnRef,
+  handleItemCardClick,
+}) => {
   return (
     <>
       {/* <Button ref={drawerTriggerBtnRef} colorScheme='teal' onClick={onOpen}>
@@ -42,7 +48,7 @@ const ListsDrawer = ({ isOpen, onOpen, onClose, drawerTriggerBtnRef }) => {
       </Button> */}
       <Drawer
         isOpen={isOpen}
-        placement='bottom'
+        placement="bottom"
         onClose={onClose}
         finalFocusRef={drawerTriggerBtnRef}
         width="screen"
@@ -51,83 +57,96 @@ const ListsDrawer = ({ isOpen, onOpen, onClose, drawerTriggerBtnRef }) => {
         className="p-0 m-0"
       >
         <DrawerOverlay />
-        <DrawerContent padding={0} margin={0} className='p-0 m-0'>
-          <DrawerCloseButton position="relative" left="90vw" top="8vh" zIndex="10" outline="1px solid black" rounded="full"/>
+        <DrawerContent padding={0} margin={0} className="p-0 m-0">
+          <DrawerCloseButton
+            position="relative"
+            left="90vw"
+            top="8vh"
+            zIndex="10"
+            outline="1px solid black"
+            rounded="full"
+          />
 
           <DrawerBody padding={0} mt="2rem">
-            <Lists />
+            <Lists handleItemCardClick={handleItemCardClick} />
           </DrawerBody>
-
         </DrawerContent>
       </Drawer>
     </>
-  )
-}
+  );
+};
 
 const FabricCanvas = (props) => {
-    const [canvas, setCanvas] = useState();
+  const [canvas, setCanvas] = useState();
 
-    const { isOpen: isAlertDialogOpen, onOpen: onAlertDialogOpen, onClose: onAlertDialogClose } = useDisclosure()
-    const cancelRef = React.useRef()
+  const {
+    isOpen: isAlertDialogOpen,
+    onOpen: onAlertDialogOpen,
+    onClose: onAlertDialogClose,
+  } = useDisclosure();
+  const cancelRef = React.useRef();
 
-    const { isOpen: isDrawerOpen, onOpen: onDrawerOpen, onClose: onDrawerClose } = useDisclosure()
-    const drawerTriggerBtnRef = React.useRef()
+  const {
+    isOpen: isDrawerOpen,
+    onOpen: onDrawerOpen,
+    onClose: onDrawerClose,
+  } = useDisclosure();
+  const drawerTriggerBtnRef = React.useRef();
 
-    useEffect(() => {
-        const c = new fabric.Canvas("canvas", {
-          height: props.canvasHeight - 175,
-          width: props.canvasWidth,
-          backgroundColor: "white",
-        });
+  useEffect(() => {
+    const c = new fabric.Canvas("canvas", {
+      height: props.canvasHeight - 175,
+      width: props.canvasWidth,
+      backgroundColor: "white",
+    });
 
-        setCanvas(c);
-        return () => {
-          c.dispose();
-        };
-      }, []);
-
-    const addItem = (canvas, item) => {
-        fabric.Image.fromURL(item, (img) => {
-            img.set({
-              left: 70,
-              top: 250,
-              angle: 0,
-              padding: 0,
-              cornersize: 10,
-              hasRotatingPoint: true,
-
-                scaleX: 0.28,
-                scaleY: 0.28,
-
-            });
-            canvas.add(img);
-          });
+    setCanvas(c);
+    return () => {
+      c.dispose();
     };
+  }, []);
 
-    const deleteSelected = (canvas) => {
-        const activeObject = canvas.getActiveObject();
-        if (activeObject) {
-          canvas.remove(activeObject);
-        }
+  const addItem = (canvas, item) => {
+    fabric.Image.fromURL(item, (img) => {
+      img.set({
+        left: 70,
+        top: 250,
+        angle: 0,
+        padding: 0,
+        cornersize: 10,
+        hasRotatingPoint: true,
+
+        scaleX: 0.28,
+        scaleY: 0.28,
+      });
+      canvas.add(img);
+    });
+  };
+
+  const deleteSelected = (canvas) => {
+    const activeObject = canvas.getActiveObject();
+    if (activeObject) {
+      canvas.remove(activeObject);
     }
+  };
 
-    const deleteAll = (canvas) => {
-        canvas.clear();
-    }
+  const deleteAll = (canvas) => {
+    canvas.clear();
+  };
 
-    // TODO: Replace with Aditya's menu, which sould return an item to add
-    const items = [item1, item2, item3, item4, item5];
-    const randomItem = items[Math.floor(Math.random() * items.length)];
-    const temp = () => {
-      // TODO: Replace with Aditya's menu, which sould return an item to add
-      addItem(canvas, randomItem);
-    }
+  // TODO: Replace with Aditya's menu, which sould return an item to add
+  const items = [item1, item2, item3, item4, item5];
+  const randomItem = items[Math.floor(Math.random() * items.length)];
+  const handleItemCardClick = (itemImageLink) => {
+    addItem(canvas, itemImageLink);
+    isDrawerOpen(false);
+  };
 
-    return (
+  return (
     <div>
       <canvas id="canvas" />
 
-      <button 
+      <button
         style={{
           position: "absolute",
           top: "60px",
@@ -138,10 +157,16 @@ const FabricCanvas = (props) => {
         onClick={onDrawerOpen}
         ref={drawerTriggerBtnRef}
       >
-        <b style={{fontSize: "25px", margin: 20}}>+</b> Add Clothes
+        <b style={{ fontSize: "25px", margin: 20 }}>+</b> Add Clothes
       </button>
 
-      <ListsDrawer isOpen={isDrawerOpen} onOpen={onDrawerOpen} onClose={onDrawerClose} drawerTriggerBtnRef={drawerTriggerBtnRef} />
+      <ListsDrawer
+        isOpen={isDrawerOpen}
+        onOpen={onDrawerOpen}
+        onClose={onDrawerClose}
+        drawerTriggerBtnRef={drawerTriggerBtnRef}
+        handleItemCardClick={handleItemCardClick}
+      />
 
       <div
         style={{
@@ -152,15 +177,21 @@ const FabricCanvas = (props) => {
           justifyContent: "left",
         }}
       >
-        <button 
-          style={{ fontSize: "25px", marginRight: 20}}
+        <button
+          style={{ fontSize: "25px", marginRight: 20 }}
           onClick={() => onAlertDialogOpen()}
-        > ↻ </button>
+        >
+          {" "}
+          ↻{" "}
+        </button>
 
-        <button 
-          style={{ fontSize: "21px", }}
+        <button
+          style={{ fontSize: "21px" }}
           onClick={() => deleteSelected(canvas)}
-        > 🗑 </button>
+        >
+          {" "}
+          🗑{" "}
+        </button>
       </div>
 
       <AlertDialog
@@ -170,26 +201,32 @@ const FabricCanvas = (props) => {
       >
         <AlertDialogOverlay>
           <AlertDialogContent>
-            <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
               Clear Canvas?
             </AlertDialogHeader>
 
             <AlertDialogBody>
-              Are you sure?  You can't undo this action afterwards.
+              Are you sure? You can't undo this action afterwards.
             </AlertDialogBody>
 
             <AlertDialogFooter>
               <Button ref={cancelRef} onClick={onAlertDialogClose}>
                 Cancel
               </Button>
-              <Button colorScheme='red' onClick={() => {onAlertDialogClose(); deleteAll(canvas);}} ml={3}>
+              <Button
+                colorScheme="red"
+                onClick={() => {
+                  onAlertDialogClose();
+                  deleteAll(canvas);
+                }}
+                ml={3}
+              >
                 Clear
               </Button>
             </AlertDialogFooter>
           </AlertDialogContent>
         </AlertDialogOverlay>
       </AlertDialog>
-
 
       <Box
         width={props.canvasWidth}
@@ -200,12 +237,15 @@ const FabricCanvas = (props) => {
         paddingTop={1}
         marginBottom={10}
       >
-        <Text 
+        <Text
           textAlign={"left"}
           fontWeight={"bold"}
           color={"gray.600"}
           marginBottom={2}
-        > Suggested </Text>
+        >
+          {" "}
+          Suggested{" "}
+        </Text>
         <Swiper
           slidesPerView={3}
           spaceBetween={30}
@@ -218,23 +258,18 @@ const FabricCanvas = (props) => {
         >
           {items.map((item, index) => (
             <SwiperSlide key={index}>
-              <img 
-                src={item} 
-                alt="item" 
-                style={{ width: "100%", height: "100%" }} 
+              <img
+                src={item}
+                alt="item"
+                style={{ width: "100%", height: "100%" }}
                 onClick={() => addItem(canvas, item)}
               />
             </SwiperSlide>
           ))}
         </Swiper>
       </Box>
-
-
-
-
-
     </div>
-    );
+  );
 };
 
 export default FabricCanvas;
