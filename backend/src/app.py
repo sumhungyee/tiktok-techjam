04fe92132f0a669855480bfa62159ebd8af74626
Shang Yi Ku <kushangyi2001@gmail.com>
2024-07-06 16:35:52 +0800

    Integrate multi-class tagging into upload endpoint

--- backend/src/app.py
+++ backend/src/app.py
@@ -7,11 +7,7 @@ from src.models.model_operations import (load_PIL_image_from_bytes,
                                          remove_background,
                                          create_small_thumbnail_base64,
                                          generate_image_hash,
-                                         load_model,
-                                         classify_processed_image,
-                                         get_classes,
-                                         get_broad,
-                                         get_material)
+                                         load_model, get_processed_image_tags)
 
 
 app = FastAPI()
@@ -21,7 +17,7 @@ app.add_middleware(
     allow_methods=["GET", "POST"],
     allow_headers=["*"]
 )
-classification_model, processor = load_model()
+model, processor = load_model()
 
 
 @app.get("/user/{user_id}/wardrobe")
@@ -119,47 +115,11 @@ async def upload_user_item(user_id: int, file: UploadFile = File(...)):
             return
         item_id = db.add_item(image_hash)
         db.add_item_to_user_wardrobe(user_id, item_id)
-        processed : bytes = remove_background(image_data, resize=True)
-        # TAGS GENERATED HERE
-
-        # for clarity: model understands "Clothing tops" more than "tops"
-        tags: list[str] = [] # user side
-        broad_class_dict = get_broad()
-        broad_class_for_clip = list(broad_class_dict.keys())
-        tags.append(
-            broad_class_dict[
-                classify_processed_image(load_PIL_image_from_bytes(processed), model, processor, broad_class_for_clip)[0]
-            ]
-        )
-        if tags[0] not in ["Accessories", "Swimwear"]:
-
-            tags.append(
-                classify_processed_image(load_PIL_image_from_bytes(processed), model, processor, get_classes())[0]
-            )
-
-            #### UNSURE ABOUT THIS, CAN DELETE
-            # materials_dict = get_material()
-            # materials_for_clip = list(materials_dict.keys())
-            # tags.append(
-            #     materials_dict[
-            #         classify_processed_image(load_PIL_image_from_bytes(processed), model, processor, materials_for_clip)[0]
-            #     ]
-            # )
-            ####
-
-        # `tags` CONTAINS YOUR TAGS
-
-
-        thumbnail = create_small_thumbnail_base64(
-            load_PIL_image_from_bytes(processed)
-        )
-        db.set_item_processed_image(item_id, processed, thumbnail)
-        processed = remove_background(image_data)
+        processed = remove_background(image_data, resize=True)
         processed_pil = load_PIL_image_from_bytes(processed)
+        tags = get_processed_image_tags(processed_pil, model, processor)
         thumbnail = create_small_thumbnail_base64(processed_pil)
-        tags, _ = classify_processed_image(processed_pil, classification_model,
-                                           processor)
-        db.update_item_details(item_id, processed, thumbnail, [tags])
+        db.update_item_details(item_id, processed, thumbnail, tags)
 
 
 @app.get("/user/{user_id}/item/{item_id}/status")
