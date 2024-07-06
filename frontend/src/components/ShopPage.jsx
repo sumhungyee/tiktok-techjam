a70644fa77a2ed2e1c1a62fef0499d115284a675
yizhenpong <yizhenpong@gmail.com>
2024-07-06 23:01:21 +0800

    change text to virtual dressing room

--- frontend/src/components/ShopPage.jsx
+++ frontend/src/components/ShopPage.jsx
@@ -61,7 +61,7 @@ const ShopPage = (props) => {
                     }}
                     onClick={() => { navigate('/fabric-canvas') }}
                 >
-                    Go To Wardrobe!
+                    Go To Virtual Dressing Room!
                 </Button>
             </Stack>  
         </>
